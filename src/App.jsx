4d82d9f130384fab1f334889dd910979b024b2cf
Xoderaman <aman2509shaikh@gmail.com>
2024-09-16 19:14:47 +0530

    Inital message

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+
+const App = () => {
+  return <div className="app">
+
+    <Navbar/>
+
+
+
+
+
+
+  </div>;
+};
+
+export default App;
